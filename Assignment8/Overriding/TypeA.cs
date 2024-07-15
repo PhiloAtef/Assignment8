8f25f638ae402415fd4594ab71185b89dd0e6c53
Philo <philoatefdev@gmail.com>
2024-07-15 20:39:52 +0300

    Add project files.

---
+++ Assignment8/Overriding/TypeA.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment8.Overriding
+{
+    internal class TypeA
+    {
+        public int A { get; set; }
+
+        public TypeA(int _a)
+        { 
+            A = _a;
+        }
+
+        public void MyFunc01()
+        {
+            Console.WriteLine("I am base");
+        }
+
+        public void MyFunc02()
+        {
+            Console.WriteLine($"TypeA = {A}");
+        }
+    }
+}
---
+++ Assignment8/Program.cs
@@ -0,0 +1,38 @@
+﻿using Assignment8.Overriding;
+
+namespace Assignment8
+{
+    internal class Program
+    {
+        #region Polymorphism - Overloading
+        //public static int Sum(int x, int y) 
+        //{
+        //    return x + y;
+        //}
+
+        //public static int Sum(int x, int y, int z)
+        //{
+        //    return x + y + z;
+        //}
+
+        //public static int Sum(int x, int y, int z, int d)
+        //{
+        //    return x + y + z + d;
+        //} 
+        #endregion
+
+        static void Main(string[] args)
+        {
+            #region Polymorphism - Overloading
+            //int result = Sum(1, 2,3,4);
+            //Console.WriteLine(result); 
+            #endregion
+
+            TypeA typeA = new TypeA(1);
+            typeA.A = 2;
+
+            typeA.MyFunc01();
+            typeA.MyFunc02();
+        }
+    }
+}
